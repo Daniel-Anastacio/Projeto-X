a2725ebd2a8349d79b225f3931c331be0e0363d3
Daniel-Anastacio <dandangomsc18@gmail.com>
2023-04-21 21:35:10 -0300

    criação da página inicial do HTML,CSS e JS, problemas com CSS e JS estudar eventos em JS(DOM) e desenvolver um design para o site posicionamento do login terminado

--- public/loginscript.js
+++ public/loginscript.js
@@ -1,6 +1,11 @@
 var email = document.getElementById('email');
 var senha = document.getElementById('senha');
+var enviar = document.getElementById("enviar");
 
 email.addEventListener('focus', ()=>{
     email.style.borderColor('#121212')
+});
+
+enviar.addEventListener("click", ()=>{
+    console.log('hhhahhaalkalajlajlak')
 });
