4499c8140cfc6c98393317e9513df91a0835bba8
Daniel-Anastacio <dandangomsc18@gmail.com>
2023-05-14 18:14:43 -0300

    slider automático

--- app/public/javascripts/initial.js
+++ app/public/javascripts/initial.js
@@ -63,3 +63,15 @@ btnCloseRegister.addEventListener('click',()=>{
         wrapper.style.display = 'flex';
     }
 });
+
+//carousel
+var count = 1
+document.getElementById('radio'+count).checked = true;
+setInterval(function(){nextImage()}, 3000 )
+function nextImage(){
+    count++;
+    if (count>4){
+        count = 1;
+    }
+    document.getElementById('radio'+count).checked = true;
+} 
